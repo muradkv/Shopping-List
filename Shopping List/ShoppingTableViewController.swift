2a1a0ac80133fe779aadcc42c259a4434706c2aa
murad <muradkv@gmail.com>
2019-05-06 20:26:12 +0300

    Initial commit

---
+++ Shopping List/ShoppingTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShoppingTableViewController.swift
+//  Shopping List
+//
+//  Created by murad on 06/05/2019.
+//  Copyright © 2019 murad. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
